f556280200326f04e644b763a4b91fc6e8fe462b
Ji Wang <traceyw@vt.edu>
2018-11-28 15:30:29 -0500

    add rspec

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,6 @@
-class Project
-	attr_accessor :tasks, :due_date
-	def initialize 
-		@tasks = []
-	end
+class Project < ActiveRecord::Base
+	has_many :tasks
+	
 	def incomplete_tasks
 		tasks.reject(&:complete?)
 	end
@@ -10,16 +8,16 @@ class Project
 		tasks.reject(&:complete?).empty?
 	end
 	def total_size
-		tasks.sum(&:size)
+		tasks.to_a.sum(&:size)
 	end
 	def remaining_size
 		incomplete_tasks.sum(&:size)
 	end
 	def completed_velocity
-		tasks.sum(&:points_toward_velocity)
+		tasks.to_a.sum(&:points_toward_velocity)
 	end
 	def current_rate
-		completed_velocity* 1.0 / 21
+		completed_velocity* 1.0 / Project.velocity_length_in_days
 	end
 	def projected_days_remaining
 		remaining_size / current_rate
